f4b112f6e31765a610bedca63c0c922dac130526
finnhittson <hittson.finn@gmail.com>
2023-04-28 11:08:26 -0400

    commit beat detection source files

---
+++ hello-world/src/app/beatdetection/OSS.py
@@ -0,0 +1,79 @@
+from scipy.io import wavfile
+import scipy
+import numpy as np
+import math
+import plotters
+
+# (1) Overlap
+def overlap(filepath, framesize:int=1024, hop:int=128):
+	sr, data = read_wav(filepath)
+	#plotters.plot_signal(data, sr)
+	frames = get_frames(data, framesize, hop)
+	#plotters.plot_frames(f, title="Frame")
+	return frames
+
+def read_wav(filepath):
+    sr, data = wavfile.read(filepath)
+    return sr, data
+
+def get_frames(data, framesize, hop):
+    frames = []
+    tmp = []
+    for i in range(len(data)):
+        tmp.append(data[i])
+        if len(tmp) == framesize:
+            frames.append(tmp)
+            tmp = tmp[hop:]
+    return np.array(frames)
+
+
+# (2) Log Power Spectrum
+def low_power_spectrum(frames):
+	tappered_frames = hamming_window(frames)
+	fft_frames = np.fft.fft(tappered_frames)
+	log_power = np.array(comp_log_power(fft_frames))
+	return log_power, fft_frames
+
+def hamming_window(frames):
+    window = scipy.signal.hamming(M=len(frames[0]))
+    return np.array([frame*window for frame in frames])
+
+def comp_log_power(fft_frames):
+    return np.array([[math.log(1+1000*abs(comp)) for comp in frame] for frame in fft_frames])
+
+
+# (3) Flux
+def comp_flux(log_power):
+	flux = np.zeros(len(log_power))
+	for n in range(1, len(log_power)):
+		for k in range(len(log_power[n])):
+			if abs(log_power[n,k]) > abs(log_power[n-1,k]):
+				add = log_power[n,k] - log_power[n-1,k]
+				flux[n] += add
+	return flux
+
+def comp_flux_author(log_power):
+	flux = np.zeros(log_power.shape[0])
+	prev = np.zeros(log_power.shape[1])
+	for i in range(log_power.shape[0]):
+		diff = log_power[i] - prev
+		diff_reduced = diff[1:]
+		diff_clipped = diff_reduced.clip(min=0)
+		prev = np.copy(log_power[i])
+		flux[i] = sum(diff_clipped)
+	return flux
+
+
+# (4) Low-pass Filter
+def low_pass_filter(flux):
+	b = scipy.signal.firwin(numtaps=15, cutoff=7, fs=344.5)
+	y = np.zeros(len(flux))
+	for n in range(len(flux)):
+		for i in range(15):
+			if n > i:
+				y[n] += b[i]*flux[n-i]
+	return y
+
+def low_pass_filter_author(flux):
+	b = scipy.signal.firwin(numtaps=15, cutoff=6/44100/128/2)
+	return scipy.signal.lfilter(b, 1.0, flux)
---
+++ hello-world/src/app/beatdetection/detectbeat.py
@@ -0,0 +1,50 @@
+import OSS as oss
+import BPD as bpd
+import accumulate as acc
+import argparse
+import scipy
+import numpy as np
+
+def run_beat_detection(filepath, framesize, hop):
+	# (1) OSS
+	frames = oss.overlap(filepath=filepath, framesize=framesize, hop=hop)
+	log_power, fft_frames = oss.low_power_spectrum(frames)
+	flux = oss.comp_flux(log_power=log_power)
+	filtered_signal = oss.low_pass_filter(flux)
+
+	# (2) BPD
+	oss_frames = bpd.overlap(data=flux, framesize=2048, hop=hop)
+	Am = bpd.generalized_autocorrelation(frames=oss_frames, c=0.5)
+	#EAC = enhance_harmonics(A=Am)
+	indices, values = bpd.pick_peaks(A=Am)
+	Lm = bpd.evaluate_pulse_train(peaks=indices[0], frame=oss_frames[0])
+
+	# (3) Accumulation
+	Lms = acc.eval_entire_signal(indices, values)
+	y = acc.accumulate_gauss(Lms=Lms)
+	idxs, vals = bpd.find_local_maximums(np.concatenate((np.zeros(98), y[98:])))
+
+	return 344.5*60/idxs[-1]
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Run beat detection algorithm on a song")
+	parser.add_argument(
+		"--path",
+		metavar="path",
+		type=str,
+		help="Path to song file.")
+	parser.add_argument(
+		"--framesize",
+		metavar="framesize",
+		type=int,
+		help="Size of framse to spilt data on.")
+	parser.add_argument(
+		"--hop",
+		metavar="hop",
+		type=int,
+		help="Hop size when splitting data.")
+
+	parser.set_defaults(framesize=1024, hop=128)
+	args = parser.parse_args()
+	run_beat_detection(args.path, args.framesize, args.hop)
