7965914fe0aec4300c796440e040a1c24169dcb4
wgraham109 <83312360+wgraham109@users.noreply.github.com>
2023-05-01 00:05:33 -0400

    Updated Logistic Regression Model

--- hello-world/src/app/server/scripts/LogisticRegression.py
+++ hello-world/src/app/server/scripts/LogisticRegression.py
@@ -1,41 +1,19 @@
 import sys
 import pandas as pd
 import numpy as np
+from random import choice
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import SongRecNew as sr
 
-def linear_regression(data):
+def logistic_regression(data, pca_var, final_dat):
 
-    col_names = ['popularity', 'acousticness', 'danceability', 'duration_ms', 'energy', 'instrumentalness',  'liveness', 'loudness','speechiness','tempo','valence','liked_by_user']
-    # load dataset
-    df = pd.read_csv('PythonRecs/SpotifyFeatures.csv')
-    cleaned_df = sr.data_clean(df)
-    final_df = cleaned_df[~cleaned_df['track_id'].isin(data)]
+    reduced_df = final_dat[~final_dat['track_id'].isin(data)]
+    final_df = reduced_df.drop(columns=['track_id'],axis=1,inplace=False)
 
-    temp = final_df.drop(columns=['track_id','artist_name', 'track_name', 'time_signature','mode','genre','key'],axis=1,inplace=False)
-    pca = PCA(n_components=12)
-    ss, pca, temp = sr.run_pca(temp, 12)
-    song_std = ss.transform(search_song)
-    song1 = pca.transform(song_std)
-
-    #Change this for output of top songs
-    temp = sr.vec_cos(song1,temp,data_og)
-    ids = list(set(temp.head(20).loc[:, 'track_id'].to_list()))
-    scores = list(set(temp.head(20).loc[:,'sim_score'].to_list()))
-    print(','.join(ids))
-    print(','.join([str(s) for s in scores]))
-
-
-    #split dataset in features and target variable
-    feature_cols = ['popularity', 'acousticness', 'danceability', 'duration_ms', 'energy', 'instrumentalness',  'liveness', 'loudness','speechiness','tempo','valence']
-    X = cleaned_df[feature_cols] # Features
-    y = cleaned_df.liked_by_user # Target variable
-
-    # split X and y into training and testing sets
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=16)
+    df_cp = data.drop(columns=['track_id'],axis=1,inplace=False)
+    X_train, X_test, y_train, y_test = train_test_split(df_cp.drop('liked_by_user', axis=1), df_cp['liked_by_user'], test_size=0.2, random_state=42)
 
     # instantiate the model (using the default parameters)
     logreg = LogisticRegression(random_state=16)
@@ -43,19 +21,86 @@ def linear_regression(data):
     # fit the model with data
     logreg.fit(X_train, y_train)
 
-    y_pred = logreg.predict_proba(X_test.head(50))
-    y_pred[:,1],y_test.head(50)
+    reduced_df['Logistic Regression'] = logreg.predict_proba(final_df)[:,1]
+    print(sum(reduced_df['Logistic Regression'].tolist()))
 
+    ret_songs_3 = reduced_df[['track_id','Logistic Regression']].sort_values(by=['Logistic Regression'], ascending=False)
+    print(ret_songs_3)
+    return(ret_songs_3)
 
-    np.sum(np.dot(y_pred,y_train))/np.size(y_pred)
-
-    return y_pred
 
 def main():
 
-    data = pd.read_csv('PythonRecs/SpotifyFeatures.csv')
-    # args = sys.argv[1:]
-    print(linear_regression(data))
+    #test data
+#     args = ['0OUKJBWS2IhHmVIxACwZzp', '53XBXgtdqf1gmWMm3rqV27',
+#   '27L8sESb3KR79asDUBu8nW', '4UDmDIqJIbrW0hMBQMFOsM',
+#   '7ACxUo21jtTHzy7ZEV56vU', '1fDsrQ23eTAVFElUMaf38X',
+#   '6FBmHx1FuaSnTnnnaThgbF', '2kR3B09M6KeJnchOkxwszt',
+#   '6QDbGdbJ57Mtkflsg42WV5', '54eZmuggBFJbV7k248bTTt',
+#   '0UOxp1BpnD8uPQMKU4wKjz', '7Cp69rNBwU0gaFT8zxExlE',
+#   '4bHsxqR3GMrXTxEPLuK5ue', '4ECNtOnqzxutZkXP4TE3n3',
+#   '1jDJFeK9x3OZboIAHsY9k2', '2grjqo0Frpf2okIBiifQKs',
+#   '7x8dCjCr0x6x2lXKujYD34', '5QTxFnGygVM4jFQiBovmRo',
+#   '0ST6uPfvaPpJLtQwhE6KfC', '43DeSV93pJPT4lCZaWZ6b1',
+#   '3yrSvpt2l1xhsV9Em88Pul', '1xsYj84j7hUDDnTTerGWlH',
+#   '1QEEqeFIZktqIpPI4jSVSF', '1UBQ5GK8JaQjm5VbkBZY66',
+#   '5IPJsGFKtxKDPCkT8lhEjN', '5WTxbyWTpoqhdxEN2szOnl',
+#   '3dCKwOvBK9MTIw2QWM59Le', '1nmZ8yqKkfooOuYvtFctDp',
+#   '12yHvSYFXI7PGzNecUvIDu', '5EWPGh7jbTNO2wakv8LjUI',
+#   '2oSpQ7QtIKTNFfA08Cy0ku', '2SiXAy7TuUkycRVbbWDEpo',
+#   '72ahyckBJfTigJCFCviVN7', '0oerlffJSzhRVvtDfLcp3N',
+#   '4aVuWgvD0X63hcOCnZtNFA', '0S3gpZzlT9Hb7CCSV2owX7',
+#   '7w6PJe5KBPyvuRYxFkPssC', '4JfuiOWlWCkjP6OKurHjSn',
+#   '6gQUbFwwdYXlKdmqRoWKJe', '7N3PAbqfTjSEU1edb2tY8j',
+#   '5Z8EDau8uNcP1E8JvmfkZe', '1gzIbdFnGJ226LTl0Cn2SX',
+#   '6eN1f9KNmiWEhpE2RhQqB5']
+    args = sys.argv[1].split(',')
+
+    liked = args[0].split(',') #UPDATE FOR PROPER IO
+    #df = pd.read_csv('user1_PCA.csv')
+    #df_cp = df.drop(columns=['track_id'],axis=1,inplace=False)
+    df = pd.read_csv('SpotifyFeatures.csv')
+    df_og = df.copy()
+    df = sr.data_clean(df)
+    songs = [choice(liked) for _ in range(5)]
+    temp_s = []
+    df_1 = df.drop(columns=['track_id','artist_name', 'track_name', 'time_signature','mode','genre','key'])
+    scaler_1, pca_1, reduced_1 = sr.run_pca(df_1,8)
+    #find the songs in the data
+    for i in songs:
+        temp = df.loc[(df['track_id'] == i)]
+        temp = temp.drop(columns=['track_id','artist_name', 'track_name', 'time_signature','mode','genre','key'])
+        if len(temp)>0:
+            song_std = scaler_1.transform(temp)
+            song1 = pca_1.transform(song_std)
+        #song1['liked_by_user'] = [0]*len(song1['1'])
+            temp_s.append(song1[0])
+    
+    #Gen song dislikes
+    dislikes = []
+    for i in temp_s: 
+        dislikes+=(list(set(sr.least_vec_cos(i,reduced_1, df).head(20).loc[:, 'track_id'].to_list())))
+
+    #Make data to format to be used
+    final = pd.read_csv('SpotifyFeatures.csv')
+    test = np.concatenate((args, dislikes))
+    #filtered_df = final[final['track_id'].isin(test)]
+    #filtered_df['liked_by_user'] = filtered_df['track_id'].isin(args).astype(int)
+    reduced = pd.DataFrame(reduced_1)
+    reduced_2 = reduced.join(final['track_id'], on=final.index)
+    filtered_df_1 = reduced_2[final['track_id'].isin(test)]
+    filtered_df_1['liked_by_user'] = filtered_df_1['track_id'].isin(args).astype(int)
+
+    print(temp)
+
+    temp = logistic_regression(filtered_df_1,pca_1, reduced_2)
+    ids, scores = [], []
+    for id, score in zip(temp.head(20).loc[:, 'track_id'], temp.head(20).loc[:, 'Logistic Regression']):
+        if not id in ids:
+            ids.append(id)
+            scores.append(str(score))
+    print(','.join(ids))
+    print(','.join(scores))
     
 
 if __name__ == '__main__':
